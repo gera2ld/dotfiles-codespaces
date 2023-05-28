b9309e49b43c9f701e88da788352048f4f3a28b8
Gerald <code@gera2ld.space>
2023-05-28 13:15:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ cd $HOME
 
 curl -fsSLo sun https://sun-gerald-win.ipns.dweb.link/sun-linux-amd64
 chmod +x sun
-SUN_MODE=minimal ./sun install neovim
-SUN_MODE=minimal ./sun install deno
+SUN_MODE=minimal ./sun install neovim deno
 
 cat <<EOF >> .zshrc
 set -o vi
